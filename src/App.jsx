9d4baf92ead40d67abbfb11952674a5e14e60c3a
TheErujeje <ucheohanado@gmail.com>
2024-08-02 09:59:42 +0100

    make other changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { AiFillApple } from "react-icons/ai";
 import Header from './components/Header'
 import LessonsInsight from './components/LessonsInsight';
 import Footer from './components/Footer';
-import Goodmorning from './components/goodmorning';
+import Footerlist from './components/Footerlist';
+
 
 function App() {
   return (<>
-  <Goodmorning />
-    {/* <Header />
+    <Header />
     <LessonsInsight />
 
     <div className='text-3xl font-bold underline px-10 pt-[84px] ' > <AiFillApple /> fHello World</div>
-    <Footer /> */}
+    <Footer />
   </>
   );
 }
