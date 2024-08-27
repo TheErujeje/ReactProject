facf1346d2c94bbe6cf24df3136828dc5fa61fb3
TheErujeje <ucheohanado@gmail.com>
2024-08-27 16:17:04 +0100

    Saving current progress.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Header from './components/Header'
 import LessonsInsight from './components/LessonsInsight';
 import Footer from './components/Footer';
 import Footerlist from './components/Footerlist';
+import OurClient from './components/OurClient';
+import ManageCommunity from './components/ManageCommunity';
 
 
 function App() {
   return (<>
     <Header />
     <LessonsInsight />
-
+    <OurClient />
+    <ManageCommunity />
     <div className='text-3xl font-bold underline px-10 pt-[84px] ' > <AiFillApple /> fHello World</div>
     <Footer />
   </>
